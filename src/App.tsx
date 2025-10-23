f2f73503089906a74a9e01acea312c0d62bca381
Alexandros Polymeros <alpolymer@gmail.com>
2025-10-23 12:57:37 +0300

    feat(components): add ArrowFunctionalComponentWithTwoProps.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ClassComponent from "./components/ClassComponent.tsx";
 import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithTwoProps from "./components/ArrowFunctionalComponentWithTwoProps.tsx";
 
 function App() {
 
@@ -12,6 +13,10 @@ function App() {
             <ArrowFunctionalComponent/>
             <ArrowFunctionalComponentWithProps title={"I am an arrow functional components with props"}/>
             <ArrowFunctionalComponentWithProps title={"I am 2nd arrow functional components with props"}/>
+            <ArrowFunctionalComponentWithTwoProps
+                title={"I am an arrow functional components with  2 props"}
+                description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, vero?"}
+            />
         </>
 
     )
---
+++ src/components/ArrowFunctionalComponentWithTwoProps.tsx
@@ -0,0 +1,19 @@
+type PropA = {
+    title: string;
+}
+
+type PropB = {
+    description: string;
+}
+
+type Props = PropA & PropB;
+const ArrowFunctionalComponentWithTwoProps = ({title, description}:Props) => {
+    return (
+     <>
+         <h1 className="font-bold mt-12 text-center text-xl">{title}</h1>
+         <p className="text-center text-sm text-gray-600">{description}</p>
+     </>
+    );
+};
+
+export default ArrowFunctionalComponentWithTwoProps;
